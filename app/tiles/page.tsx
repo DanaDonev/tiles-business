a71e1990edca28513bdf6851afd25bcaf3392e5e
Dana <danadonevska@gmail.com>
2026-05-26 16:45:26 +0100

    error13 corrected

--- app/tiles/page.tsx
+++ app/tiles/page.tsx
@@ -1,6 +1,8 @@
 import { Suspense } from "react";
 import TilesClient from "./TilesClient";
 
+export const dynamic = "force-dynamic";
+
 export default function Page() {
   return (
     <Suspense fallback={<div>Loading...</div>}>
